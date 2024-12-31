60cddc1b7fb3ec9e16e9ec9a0990f935fee997cb
Antony Paquay <antony.paquay@gmail.com>
2024-12-31 11:33:07 +0100

    feat: add promises errors handler

--- src/app/main.js
+++ src/app/main.js
@@ -1,5 +1,9 @@
 import App from "./App.js";
 import "./config/firebase.js";
 
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
+
 const root = document.getElementById("app");
 const app = new App(root);
