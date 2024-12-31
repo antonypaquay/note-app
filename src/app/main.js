8deddb502ac67a7075bd2afdfe021c84c9307aa0
Antony Paquay <antony.paquay@gmail.com>
2024-12-31 11:48:06 +0100

    chore: update node options

--- src/app/main.js
+++ src/app/main.js
@@ -1,9 +1,5 @@
 import App from "./App.js";
 import "./config/firebase.js";
 
-process.on('unhandledRejection', (reason, promise) => {
-  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
-});
-
 const root = document.getElementById("app");
 const app = new App(root);
